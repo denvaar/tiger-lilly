3b9c9bbfc60ae240c4f3922515491ee24d2e3e94
Denver Smith <denverpsmith@gmail.com>
2017-11-28 07:51:48 -0700

    setup static uploads directory

--- lib/tiger_lilly_web/endpoint.ex
+++ lib/tiger_lilly_web/endpoint.ex
@@ -9,10 +9,7 @@ defmodule TigerLillyWeb.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :tiger_lilly, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
-
-  plug Plug.Static,
-    at: "/uploads", from: {:tiger_lilly, 'uploads'}, gzip: false
+    only: ~w(css fonts images uploads js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
